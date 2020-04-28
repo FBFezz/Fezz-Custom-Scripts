2eb6fb7e159dc29b9b0ea0be93ad21528e48071e
Fezz <43589668+FBFezz@users.noreply.github.com>
2020-04-28 04:16:07 +0100

    Add files via upload

---
+++ [custom]/fezzguns/client.lua
@@ -0,0 +1,20 @@
+RegisterCommand("clearweapons", function() -- Makes the command "clearweapons"
+    RemoveAllPedWeapons(GetPlayerPed(-1), true) -- Removes all players weapons
+    notify("~r~Cleared All weapons") -- Sends notification that weapons have been cleared
+end)
+
+Citizen.CreateThread(function()
+
+    local h_key = 74 -- "H" key
+    local x_key = 73 -- "X" key
+    while true do 
+        Citizen.Wait(1)
+        if IsControlJustReleased(1, h_key) then -- If "H" is just released then
+            print("The key ".. h_key .. " was pressed") -- Print "H" was pressed
+            giveWeapon("weapon_pistol") -- Gives Pistol
+            giveWeapon("weapon_knife") -- Gives Knife
+            alert("~b~Given Weapons with ~INPUT_VEH_HEADLIGHT~") -- Notification that h gave weapons
+        end
+    end
+
+end)
