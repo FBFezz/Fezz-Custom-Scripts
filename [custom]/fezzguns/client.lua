62642ad51778f4ab6c51d5eadff1e9aaf86fd512
Fezz <43589668+FBFezz@users.noreply.github.com>
2020-05-01 08:51:10 +0100

    Added new Car spawning and suicide function
    Also added weapon components

--- [custom]/fezzguns/client.lua
+++ [custom]/fezzguns/client.lua
@@ -3,6 +3,19 @@ RegisterCommand("clearweapons", function() -- Makes the command "clearweapons"
     notify("~r~Cleared All weapons") -- Sends notification that weapons have been cleared
 end)
 
+local cars = {"adder", "comet", "cheetah", "faggio"}
+RegisterCommand("car", function()
+
+    local car = (cars[math.random(#cars)])
+    spawnCar(car)
+    notify("~p~Spawned car: ~h~~g~" .. car)
+end)
+
+RegisterCommand("pleasedie", function()
+    SetEntityHealth(PlayerPedId(), 0)
+    notify("~r~You Died.")
+end)
+
 Citizen.CreateThread(function()
 
     local h_key = 74 -- "H" key
@@ -11,6 +24,9 @@ Citizen.CreateThread(function()
         if IsControlJustReleased(1, h_key) then -- If "H" is just released then
             print("The key ".. h_key .. " was pressed") -- Print "H" was pressed
             giveWeapon("weapon_pistol") -- Gives Pistol
+            giveWeapon("weapon_pumpshotgun")
+                weaponComponent("weapon_pumpshotgun", "COMPONENT_AT_AR_FLSH")
+                weaponComponent("weapon_pumpshotgun", "COMPONENT_AT_SR_SUPP")
             giveWeapon("weapon_knife") -- Gives Knife
             alert("~b~Given Weapons with ~INPUT_VEH_HEADLIGHT~") -- Notification that h gave weapons
         end
