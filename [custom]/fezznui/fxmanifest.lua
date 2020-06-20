fbf315075f25a929ea384614a2bc167f2c406c16
Fezz <43589668+FBFezz@users.noreply.github.com>
2020-06-20 10:50:04 +0100

    Update fxmanifest.lua

--- [custom]/fezznui/fxmanifest.lua
+++ [custom]/fezznui/fxmanifest.lua
@@ -1,24 +1,11 @@
--- Resource Metadata
 fx_version 'bodacious'
-games { 'gta5' }
+game 'gta5'
 
 author 'Fezz'
 description 'nui_example'
 version '1.0.0'
 
--- What to run
-client_scripts {
-    'main.lua'--,
- --   'client_two.lua'
-}
---server_script 'server.lua'
-
--- Extra data can be used as well
---my_data 'one' { two = 42 }
---my_data 'three' { four = 69 }
-
--- due to Lua syntax, the following works too:
---my_data('nine')({ninety = "nein"})
+client_script 'main.lua'
 
 ui_page 'html/index.html'
 
