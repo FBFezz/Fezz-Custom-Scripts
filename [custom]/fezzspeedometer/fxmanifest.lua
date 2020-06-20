4e927f37433bc5ce5bbc338aee60dc481fd0d047
Fezz <43589668+FBFezz@users.noreply.github.com>
2020-06-20 10:50:56 +0100

    Update fxmanifest.lua

--- [custom]/fezzspeedometer/fxmanifest.lua
+++ [custom]/fezzspeedometer/fxmanifest.lua
@@ -1,21 +1,9 @@
--- Resource Metadata
 fx_version 'bodacious'
-games { 'gta5' }
+game 'gta5'
 
 author 'Fezz'
 description 'Custom Speedometer'
 version '1.0.0'
 
--- What to run
-client_scripts {
-    'speedometer.c.lua'--,
- --   'client_two.lua'
-}
---server_script 'server.lua'
+client_script 'speedometer.c.lua'
 
--- Extra data can be used as well
---my_data 'one' { two = 42 }
---my_data 'three' { four = 69 }
-
--- due to Lua syntax, the following works too:
---my_data('nine')({ninety = "nein"})
